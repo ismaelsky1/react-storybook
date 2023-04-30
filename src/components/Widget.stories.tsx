48a9032f805c9b0cd7d1d4ca46fae51d6a2c53fc
ismaelsky1 <ismael.silva@viavarejo.com.br>
2023-04-29 21:49:55 -0300

    milisegundo

--- src/components/Widget.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react'
-import { Widget } from './Widget'
-
-export default {
-  title: 'Widget',
-  component: Widget,
-} as Meta
-
-export const Default: StoryObj = {}
